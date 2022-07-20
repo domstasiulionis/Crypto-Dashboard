395fd2bd8ca46d9c2d9d9a876cb71c3ad365a016
Dominykas Stasiulionis <you@example.com>
2022-07-20 03:18:56 +0100

    improved layout of sidebar. Need to change fill of icons on select

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import "./Styles/App.scss";
+import { useState } from "react";
 import Sidebar from "./Components/Sidebar";
 
+import "./Styles/App.scss";
+
 function App() {
+  const [activeIcon, setActiveIcon] = useState("home");
+
   return (
     <div className="Content">
-      <Sidebar />
+      <Sidebar activeIcon={activeIcon} setActiveIcon={setActiveIcon} />
     </div>
   );
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,50 +2,58 @@ import "../Styles/Sidebar.scss";
 
 // Icons
 import { IoHome } from "react-icons/io5";
-import { FiBarChart2 } from "react-icons/fi";
-import { AiOutlineWallet } from "react-icons/ai";
-import { GiBackwardTime } from "react-icons/gi";
-import { AiOutlineCompass } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai";
 import { FiSettings } from "react-icons/fi";
 import { AiOutlinePoweroff } from "react-icons/ai";
 
 // Images
 import logo from "../Assets/moon-logo.png";
 
-const Sidebar = () => {
-  return (
-    <div className="sidenav">
-      <img src={logo} className="sidenav__logo" alt="logo" />
-      <h3 className="logo__text logo__text--moon">MOON</h3>
-      <h3 className="logo__text logo__text--cake">CAKE</h3>
+const Sidebar = ({ activeIcon, setActiveIcon }) => {
+  const selectedHome = () => {
+    setActiveIcon("home");
+  };
 
-      {/* Top icons */}
-      <div className="logo--active">
-        <IoHome className="icon icon--active" />
-      </div>
-      <div>
-        <FiBarChart2 className="icon" />
-      </div>
-      <div>
-        <AiOutlineWallet className="icon" />
-      </div>
-      <div>
-        <GiBackwardTime className="icon" />
-      </div>
-      <div>
-        <AiOutlineCompass className="icon" />
+  const selectedFav = () => {
+    setActiveIcon("favourites");
+  };
+
+  const selectedSettings = () => {
+    setActiveIcon("settings");
+  };
+
+  return (
+    <nav className="sidenav">
+      <div className="sidenav-top-container">
+        {/* Logo */}
+        <img src={logo} className="sidenav__logo" alt="logo" />
+        <h3 className="logo__text logo__text--moon">MOON</h3>
+        <h3 className="logo__text logo__text--cake">CAKE</h3>
+
+        {/* Top icons */}
+        <IoHome
+          onClick={() => selectedHome()}
+          className={`icon ${activeIcon === "home" ? "icon--active" : ""}`}
+        />
+        <AiOutlineStar
+          onClick={() => selectedFav()}
+          className={`icon ${
+            activeIcon === "favourites" ? "icon--active" : ""
+          }`}
+        />
       </div>
 
       {/* Bottom icons */}
-      <div className="group-position--end">
-        <div>
-          <FiSettings className="icon--spin" />
-        </div>
-        <div>
-          <AiOutlinePoweroff className="icon" />
-        </div>
+      <div className="bottom-container">
+        <FiSettings
+          onClick={() => selectedSettings()}
+          className={`icon icon--spin ${
+            activeIcon === "settings" ? "icon--active" : ""
+          }`}
+        />
+        <AiOutlinePoweroff className="icon--power" />
       </div>
-    </div>
+    </nav>
   );
 };
 
