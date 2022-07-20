7be64b337efa6834db66a94cf581d7dba408b1bd
Dominykas Stasiulionis <you@example.com>
2022-07-20 17:13:44 +0100

    icons fill if selected, added border left on selected icon and completed geeral styling for sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,9 @@
 import "../Styles/Sidebar.scss";
 
 // Icons
-import { IoHome } from "react-icons/io5";
-import { AiOutlineStar } from "react-icons/ai";
-import { FiSettings } from "react-icons/fi";
-import { AiOutlinePoweroff } from "react-icons/ai";
+import { IoHome, IoHomeOutline } from "react-icons/io5";
+import { AiOutlineStar, AiFillStar } from "react-icons/ai";
+import { BsGear, BsGearFill } from "react-icons/bs";
 
 // Images
 import logo from "../Assets/moon-logo.png";
@@ -31,27 +30,59 @@ const Sidebar = ({ activeIcon, setActiveIcon }) => {
         <h3 className="logo__text logo__text--cake">CAKE</h3>
 
         {/* Top icons */}
-        <IoHome
-          onClick={() => selectedHome()}
-          className={`icon ${activeIcon === "home" ? "icon--active" : ""}`}
-        />
-        <AiOutlineStar
-          onClick={() => selectedFav()}
-          className={`icon ${
-            activeIcon === "favourites" ? "icon--active" : ""
-          }`}
-        />
+
+        {activeIcon === "home" ? (
+          <div className="icon-container--active">
+            <IoHome
+              className={`icon ${activeIcon === "home" ? "icon--active" : ""}`}
+            />
+          </div>
+        ) : (
+          <div className="icon-container">
+            <IoHomeOutline
+              onClick={() => selectedHome()}
+              className={`icon ${activeIcon === "home" ? "icon--active" : ""}`}
+            />
+          </div>
+        )}
+
+        {activeIcon === "favourites" ? (
+          <div className="icon-container--active">
+            <AiFillStar
+              className={`icon ${
+                activeIcon === "favourites" ? "icon--active" : ""
+              }`}
+            />
+          </div>
+        ) : (
+          <div className="icon-container">
+            <AiOutlineStar
+              onClick={() => selectedFav()}
+              className={`icon ${
+                activeIcon === "favourites" ? "icon--active" : ""
+              }`}
+            />
+          </div>
+        )}
       </div>
 
       {/* Bottom icons */}
-      <div className="bottom-container">
-        <FiSettings
-          onClick={() => selectedSettings()}
-          className={`icon icon--spin ${
-            activeIcon === "settings" ? "icon--active" : ""
-          }`}
-        />
-        <AiOutlinePoweroff className="icon--power" />
+      <div className="sidenav-bottom-container">
+        {activeIcon === "settings" ? (
+          <BsGearFill
+            onClick={() => selectedSettings()}
+            className={`icon icon--spin ${
+              activeIcon === "settings" ? "icon--active" : ""
+            }`}
+          />
+        ) : (
+          <BsGear
+            onClick={() => selectedSettings()}
+            className={`icon icon--spin ${
+              activeIcon === "settings" ? "icon--active" : ""
+            }`}
+          />
+        )}
       </div>
     </nav>
   );
