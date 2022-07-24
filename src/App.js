b66fab82a897364e69cce61c983f33206d285727
Dominykas Stasiulionis <domstasiulionis@gmail.com>
2022-07-24 23:13:42 +0100

    addtional sizing changes for coin card were needed due to previous screen changes in previous commit. drop down also created when user clicks on chevron of coin

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [coins, setCoins] = useState([]);
   const [update, setUpdate] = useState(0);
 
-  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d
+  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=25&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d
   `;
 
   useEffect(() => {
