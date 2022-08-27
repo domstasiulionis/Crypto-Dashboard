3b3f1e9e1c56c5f15c17ea0fd553ab1343a2b913
Dominykas Stasiulionis <domstasiulionis@gmail.com>
2022-08-27 07:15:59 +0100

    created navbar for favourites page

--- src/Components/FavNavBar.jsx
+++ src/Components/FavNavBar.jsx
@@ -1,15 +1,26 @@
-import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
+import "../Styles/FavNavBar.scss";
 
-import { IoHome } from "react-icons/io5";
 import { BsSearch } from "react-icons/bs";
-
-import "../Styles/HomeNavBar.scss";
+import { AiFillStar } from "react-icons/ai";
 
 const HomeNavBar = ({ setHamburgerMenu, setSearchText }) => {
   return (
     <nav className="fav-nav-container">
       <nav className="fav-nav">
+        <div className="fav-nav-left">
+          <div
+            className="fav-nav-hamburger"
+            onClick={() => setHamburgerMenu(true)}
+          >
+            <span className="fav-nav-hamburger__line"></span>
+            <span className="fav-nav-hamburger__line"></span>
+            <span className="fav-nav-hamburger__line"></span>
+          </div>
+          <AiFillStar className="top-nav-left__icon" />
+          <h3 className="fav-nav-left__title">Favourites</h3>
+        </div>
+
         <div className="fav-nav-middle">
           <div className="search-bar">
             <input
@@ -20,6 +31,8 @@ const HomeNavBar = ({ setHamburgerMenu, setSearchText }) => {
             <BsSearch className="search-bar__icon" />
           </div>
         </div>
+
+        {/* <div className="top-nav-right"></div> */}
       </nav>
     </nav>
   );
--- src/Pages/Favourites.jsx
+++ src/Pages/Favourites.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useCallback } from "react";
+import FavNavBar from "../Components/FavNavBar";
+
+import "../Styles/Favourites.scss";
 
 const Favourites = (activeIcon, setActiveIcon) => {
   return (
-    <div>
-      <h1>favs</h1>
+    <div className="fav-container">
+      <FavNavBar />
+      <div className="coins-container"></div>
     </div>
   );
 };
